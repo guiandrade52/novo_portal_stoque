29a240168f3fb41f40b61a802195d75e007e880b
Fagner Gomes <fagner.egomes@gmail.com>
2019-07-16 08:12:52 -0300

    Create PrivateRouter function

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Repositories from '~/pages/Repositories';
 import About from '~/pages/About';
+import { isAutenticated } from '~/services/auth';
+
+const PrivateRouter = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+    isAutenticated() ? (
+      <Component {...props} />
+    )
+    : (
+      <Redirect to={{ pathname: '/Login', state: { from: props.location } }} />
+    )
+  )}
+  />
+);
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Repositories} />
-      <Route path="/about" component={About} />
+      <PrivateRouter path="/" exact component={Repositories} />
+      <PrivateRouter path="/about" component={About} />
     </Switch>
   );
 }
